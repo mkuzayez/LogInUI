fd721c2b715daa2e674d9b8d59ef9fdefa09d2d6
unknown <mkuzayez@gmail.com>
2024-08-21 23:46:42 +0300

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,25 @@ import 'package:login_ui/log_in.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        resizeToAvoidBottomInset: true,
-        body: LogInScreen(),
-      ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.miniCenterDocked,
+          floatingActionButton: SizedBox(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 24, 0, 12),
+              child: TextButton(
+                child: const Text(
+                  "Don't have an account? Sign up",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 106, 98, 183), fontSize: 18),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
+          resizeToAvoidBottomInset: false,
+          body: const LogInScreen()),
     ),
   );
 }
